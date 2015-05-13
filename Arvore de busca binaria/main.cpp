4371f9da6e66a9b927befdb10d3c65df60bcaf42
diogommartins <diogo.martins@unirio.br>
2015-05-13 18:33:27 -0300

    Initial Commit

---
+++ Arvore de busca binaria/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arvore de busca binaria
+//
+//  Created by Diogo Martins on 5/13/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
